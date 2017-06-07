46ad7be8f6e7f31b381515ea7dced5c49e6d37a9
Hebron George <hebrontgeorge@gmail.com>
2017-06-07 15:33:34 -0400

    slightly improve average run time

--- ch1_arrays_and_strings/1.1/challenge.py
+++ ch1_arrays_and_strings/1.1/challenge.py
@@ -2,16 +2,17 @@ def has_all_unique_characters(string):
 	# First thought: go through each character, 
 	# update a dictionary where key = character, value = count
 
+	# Second thought: As soon as we see a character in the dictionary already,
+	# the function can return false. This stays at O(n) run time since if we have a 
+	# completely unique set of characters (worst case), we'll have to go through them all anyway, 
+	# but returns false at the first instance of a duplicate character.
+
 	character_dictionary = {}
 	for c in string: # This should have a runtime of O(n) where n = size of string
 		if c in character_dictionary:
-			character_dictionary[c] += 1
-		else:
-			character_dictionary[c] = 1
-
-	for key in character_dictionary:
-		if character_dictionary[key] > 1:
 			return False
+		else:
+			character_dictionary[c] = True
 
 	return True
 
